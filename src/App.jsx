fc228d1e53a1399ba0b1abeed1c8f3ae29ef93fa
davissssss <davis23232323@gmail.com>
2025-04-10 14:32:55 +0800

    build custom hook to get all the pokemons and images

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,13 @@ import { useNavigate } from "react-router-dom";
 import Pokemon from "./Pokemon";
 import logo from "./assets/International_Pokémon_logo.svg.png";
 import useCurrentUser from "./hooks/useCurrentUser";
+import useAllPokemons from "./hooks/useAllPokemons";
 
 export default function App() {
-  const [pokemons, setPokemons] = useState([]);
-  const [pokemonImages, setPokemonImages] = useState({});
+  const { pokemons, pokemonImages } = useAllPokemons();
   const [searchQuery, setSearchQuery] = useState("");
   const currentUser = useCurrentUser();
   const navigate = useNavigate();
-  
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/pokemon")
-      .then((response) => response.json())
-      .then(async (res) => {
-        const pokemonData = res.data.data;
-        setPokemons(pokemonData);
-
-        // 批量請求所有 Pokémon 的圖片
-        const imageRequests = pokemonData.map((pokemon) =>
-          fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.race}`)
-            .then((res) => res.json())
-            .then((pokeData) => ({
-              race: pokemon.race,
-              image: pokeData.sprites.other["official-artwork"].front_default,
-            }))
-        );
-
-        // 等待所有請求完成
-        const images = await Promise.all(imageRequests);
-
-        // 轉換成 { name: imageUrl } 格式
-        const imageMap = images.reduce((acc, item) => {
-          acc[item.race] = item.image;
-          return acc;
-        }, {});
-
-        setPokemonImages(imageMap);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
 
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
