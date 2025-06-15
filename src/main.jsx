c74038c26dce4602f3291b471456cfd7fd59ff06
davissssss <davis23232323@gmail.com>
2025-06-15 15:38:15 +0800

    adjust css file

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./styles/index.css";
+import "./styles/components/button.css";
+import "./styles/components/pokemonCard.css";
+import "./styles/components/loginPage.css";
+import "./styles/components/tradePage.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
